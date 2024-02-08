43de56f704c9e76994c46437985e78eefe44a9f4
Chris Dillon <chris.dillon@serialaffinity.co.uk>
2024-02-08 15:43:45 +0000

    Fixed build issue??

--- remix.config.mjs
+++
@@ -1,8 +0,0 @@
-import { config } from '@netlify/remix-adapter'
-
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ...(process.env.NODE_ENV === 'production' ? config : undefined),
-  ignoredRouteFiles: ['**/.*'],
-  serverModuleFormat: 'cjs'
-}
