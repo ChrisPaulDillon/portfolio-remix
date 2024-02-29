fb626f42de37388841c590adb5d149b14cd9dc06
Chris <chrispauldillon@live.com>
2024-02-29 20:15:24 +0000

    Increased both font sizes again

--- app/features/home/HomeBodyText.tsx
+++ app/features/home/HomeBodyText.tsx
@@ -4,7 +4,7 @@ export const HomeBodyText: React.FC<TextProps> = props => {
   return (
     <Text
       textAlign={{ base: 'center', lg: 'left' }}
-      fontSize={{ base: '2xl', lg: 'lg' }}
+      fontSize={{ base: '3xl', lg: 'lg' }}
       {...props}
     />
   )
--- app/features/shared/SectionHeading.tsx
+++ app/features/shared/SectionHeading.tsx
@@ -7,7 +7,7 @@ export const SectionHeading: React.FC<HeadingProps> = props => (
     as="h2"
     textUnderlineOffset={10}
     textAlign={{ base: 'center', sm: 'center', lg: 'left' }}
-    fontSize={{ base: '3xl', sm: '3xl', lg: '4xl' }}
+    fontSize={{ base: '4xl', sm: '4xl', lg: '4xl' }}
     {...props}
   />
 )
